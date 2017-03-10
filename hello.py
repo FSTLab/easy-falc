2f63b75cccfb845800ef00a8a7311eee5fa9b018
Thomas Roulin <thomasroulinCH@gmail.com>
2017-03-10 14:55:31 +0100

    Imp

--- hello.py
+++ hello.py
@@ -36,6 +36,7 @@ def query_db(query, args=(), one=False):
     """Query db."""
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
+    print(rv)
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
@@ -64,15 +65,21 @@ def index():
     except KeyError:
         print "No text received"
 
-    print(text)
-
     words, warnings = util.process(text)
 
-    im = ','.join(['?'] * len(words))
-    query = 'select * from Mots where mot in ({})'.format(im)
-    mots = query_db(query, [word[0].lower() for word in words], one=True)
-    for mot in mots:
-        if mot['ponderation'] < 10:
-            print(mot['mot'])
+    # TODO: Find low ponderation words. Then offer synonyms, if possible.
+    # if text is not "":
+    #     im = ','.join(['?'] * len(words))
+    #     print(im)
+    #     query = 'select * from Mots where mot in ({})'.format(im)
+    #     mots = query_db(query, [word[0].lower() for word in words])
+    #     for word, mot in zip(words, mots):
+    #         if int(mot['ponderation']) < 10.0:
+    #             index = len(warnings)
+    #             start = word[1]
+    #             snippet = word[0]
+    #             end = start + len(snippet) - 1
+    #             comment = "Utilisez des mots plus courants."
+    #             warnings.append(Warning(index, start, end, comment, snippet))
 
     return render_template('index.html.j2', text=text, warnings=warnings)
