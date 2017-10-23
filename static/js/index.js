6c8c52fbeb56a700a9a8df2b1796208adbb0f593
Thomas roulin <thomasroulinCH@gmail.com>
2017-10-23 14:33:06 +0000

    Save carret position after text update

--- static/js/index.js
+++ static/js/index.js
@@ -50,6 +50,38 @@ function isEmpty(obj) {
   return Object.keys(obj).length === 0;
 }
 
+function saveCaretPosition(context){
+    var selection = window.getSelection();
+    var range = selection.getRangeAt(0);
+    range.setStart(  context, 0 );
+    var len = range.toString().length;
+
+    return function restore(){
+        var pos = getTextNodeAtPosition(context, len);
+        selection.removeAllRanges();
+        var range = new Range();
+        range.setStart(pos.node ,pos.position);
+        selection.addRange(range);
+    };
+}
+
+function getTextNodeAtPosition(root, index){
+    var lastNode = null;
+
+    var treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT,function next(elem) {
+        if(index > elem.textContent.length){
+            index -= elem.textContent.length;
+            lastNode = elem;
+            return NodeFilter.FILTER_REJECT;
+        }
+        return NodeFilter.FILTER_ACCEPT;
+    });
+    var c = treeWalker.nextNode();
+    return {
+        node: c? c: root,
+        position: c? index:  0
+    };
+}
 
 /*************************************\
 | ************** ASYNC ************** |
@@ -92,7 +124,9 @@ function update(textarea, text, tips){
 
   TIPS = tips;
 
+  var restore = saveCaretPosition($(textarea)[0]);
   $(textarea).html(generateFormattedText(text, tips));
+  restore();
   $('#tips-container').html(generateTips(tips));
 
   // update after T was updated
